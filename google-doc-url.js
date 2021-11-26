7fec092c8674e6f7b890fed848cedf308ff05048
gamarojfarneiro <94110766+gamarojfarneiro@users.noreply.github.com>
2021-11-26 11:12:17 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/14frf8DROvo4Kv68mSi_Z13z-EeOclrQxP2BVWgdNMgg/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/14frf8DROvo4Kv68mSi_Z13z-EeOclrQxP2BVWgdNMgg/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAKim4PTcfRghnUlUMJAI4nGjsPn4uhAgA';
